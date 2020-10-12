4f9107727da350826d10458b12acad2488977021
Zen Dharma <zendharma10@gmail.com>
2020-10-13 01:51:06 +0700

    Fix typo

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let fs = require('fs')
-let JsBarcode = require('jsbarcode');
-let { createCanvas } = require('canvas');
+let JsBarcode = require('jsbarcode')
+let { createCanvas } = require('canvas')
 
 let quantity = 300
 
 for (let i = 0; i < quantity; i++) {
     let number = getRndInteger(10000001, 99999999)
-    let canvas = createCanvas();
-    JsBarcode(canvas, number);
+    let canvas = createCanvas()
+    JsBarcode(canvas, number)
     let buffer = canvas.toBuffer('image/png')
-    fs.writeFileSync(`./img/${number}.png`, buffer);
+    fs.writeFileSync(`./img/${number}.png`, buffer)
 }
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min) ) + min
 }
